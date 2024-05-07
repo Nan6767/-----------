f58f291a989025d337fdfbed9aa9cf1569ee6324
Nan6767 <noviko8n@yandex.ru>
2024-05-07 19:24:30 +0200

    venv

--- web_site/settings.py
+++ web_site/settings.py
@@ -116,12 +116,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
